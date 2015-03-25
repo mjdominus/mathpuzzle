ac48f5dec6093d3132de143fb52e95ec5dcf268d
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2015-03-24 22:31:19 -0400

    Draft of program finished
    Separate operation list generator from operation list vetting
    Just regenerate until we get an acceptable one
    Then convert operation list to strings and insert into SVG as before

--- mathpuzzle.pl
+++ mathpuzzle.pl
@@ -14,6 +14,11 @@ my @pats = ([qw(a b * x /)],
 	    [qw(* a / b x)],
 	   );
 
+my %optab = ('+' => sub { $_[0] + $_[1] },
+             '*' => sub { $_[0] * $_[1] },
+             '/' => sub { $_[0] / $_[1] }
+            );
+
 # select multipliers
 my @multipliers = select_multipliers(4, 2, $opt{M});
 
@@ -22,8 +27,12 @@ my @multipliers = select_multipliers(4, 2, $opt{M});
 # insert into %t
 
 for my $letter (qw(A B C D)) {
-  for my $number (1..5) {
-    $t{"$letter$number"} = shift @vals;
+  my @pat = @{randa(@pats)};
+  die if @pat != 5;
+  my $multiplier = shift @multipliers;
+  my @ops = gen_ops($letter, $multiplier, @pat);
+  for my $i (0 .. 4) {
+    $t{$letter . ($i+1)} = op_string($ops[$i]);
   }
 }
 
@@ -34,12 +43,61 @@ while (<>) {
   print;
 }
 
+sub gen_ops {
+  my ($letter, $multiplier, @pat) = @_;
+  my @ops;
+  do {
+    @ops = ();
+    for my $i (0 .. 4) {
+      if ($pat[$i] eq "a" || $pat[$i] eq "b" ) {
+        my $min = $pat[$i] eq "a" && $letter eq "A" ? 1 : -$opt{A};
+        push @ops, ["+", select_adder($min, $opt{A})];
+      } elsif ($pat[$i] eq "*" || $pat[$i] eq "/") {
+        push @ops, [$pat[$i], $multiplier];
+      } elsif ($pat[$i] eq "x") {
+        push @ops, ["+", -total(@ops)];
+      }
+    }
+  } until ops_acceptable(@ops);
+  return @ops;
+}
+
+sub ops_acceptable {
+  my (@ops) = @_;
+  for my $i (0 .. $#ops) {
+    return unless op_acceptable($ops[$i]);
+    my $x = total(@ops[0..$i]);
+    return unless $x == int($x);
+  }
+  return 1;
+}
+
+sub op_acceptable {
+  my ($op) = @_;
+  return 1 unless $op->[0] eq "+";
+  return 0 if $op->[1] == 0 && ! $opt{z};
+  return 0 if abs($op->[1]) > $opt{A};
+  return 1;
+}
+
+# If we start with 0, what's the total after we perform these operations?
+sub total {
+  my @ops = @_;
+  my $x = 0;
+  for my $op (@ops) {
+    $x = $optab{$op->[0]}->($x, $op->[1]);
+  }
+  return $x;
+}
+
 # select N multipliers
 # min is first
 # then min or min+1
 # then min or min+1 or min+2
 # etc.
 # but there must be one that is the Max.
+#
+# If Max is really big, maybe increase min by more than 1 each time
 sub select_multipliers {
   my ($N, $min, $Max) = @_;
   my $max = $min;
@@ -52,6 +110,14 @@ sub select_multipliers {
   }
   if ($gotMax) { push @M, randr($min, $max++) } else { push @M, $Max }
   return @M;
+}
+
+sub op_string {
+  my ($op) = @_;
+  if ($op->[0] eq "+") { return adder_string($op->[1]) }
+  elsif ($op->[0] eq "*") { return '×' . $op->[1] }
+  elsif ($op->[0] eq "/") { return '÷' . $op->[1] }
+  else { die "Unknown op->[0] = '$op->[0]'" }
 }
 
 sub adder_string {
